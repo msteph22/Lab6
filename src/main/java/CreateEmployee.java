974a9896c6d24a8353bc1552700e04d69b2a3df2
msteph22 <miller25stephens@gmail.com>
2021-03-16 14:03:24 -0500

    Lab 5

---
+++ src/main/java/CreateEmployee.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stephensmac
+ */
+public class CreateEmployee {
+    public static void main(String[] args) {
+        //Create Objects
+        Employee empOne = new Employee();
+        Employee empTwo = new Employee();
+        Employee empThree = new Employee();
+        
+        //Set values inside objects
+        empOne.setName("Susan Meyers");
+        empOne.setIdNumber(47899);
+        empOne.setDepartment("Accounting");
+        empOne.setPosition("Vice President");
+        
+        empTwo.setName("Mark Jones");
+        empTwo.setIdNumber(39119);
+        empTwo.setDepartment("IT");
+        empTwo.setPosition("Programmer");
+        
+        empThree.setName("Joy Rogers");
+        empThree.setIdNumber(81774);
+        empThree.setDepartment("Manufacturing");
+        empThree.setPosition("Engineer");
+        
+        
+        //Display Objects
+        
+        System.out.println("Name: " + empOne.getName());
+        System.out.println("ID: " + empOne.getIdNumber());
+        System.out.println("Department: " + empOne.getDepartment());
+        System.out.println("Position: " + empOne.getPosition());
+        
+        System.out.println("");
+        
+        System.out.println("Name: " + empTwo.getName());
+        System.out.println("ID: " + empTwo.getIdNumber());
+        System.out.println("Department: " + empTwo.getDepartment());
+        System.out.println("Position: " + empTwo.getPosition());
+        
+        System.out.println("");
+        
+        System.out.println("Name: " + empThree.getName());
+        System.out.println("ID: " + empThree.getIdNumber());
+        System.out.println("Department: " + empThree.getDepartment());
+        System.out.println("Position: " + empThree.getPosition());
+    }
+}
---
+++ src/main/java/Employee.java
@@ -0,0 +1,72 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stephensmac
+ */
+
+//Write a class named Employee
+public class Employee {
+    //following fields
+    private String name;
+    private int idNumber;
+    private String department;
+    private String position;
+    
+    //following constructors
+    //A constructor that accepts the following values as arguments and assigns 
+    //them to the appropriate fields: employee's name, employee's ID number, department, and position
+    public Employee(String name, int idNumber, String department, String position){
+        this.name = name;
+        this.idNumber = idNumber;
+        this.department = department;
+        this.position = position;
+    }
+    //A constructor that accepts the following values as arguments and assigns
+    //them to the appropriate fields: employee's name and ID number. The department
+    //and position fields should be assigned an empty string ("")
+    
+    
+    //A no-arg constructor that assigns empty strings ("") to the name, department,
+    //and position fields, and 0 to the idNumber field;
+    public Employee(){
+        name = "";
+        idNumber = 0;
+        department = "";
+        position = "";
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public void setIdNumber(int idNumber){
+        this.idNumber = idNumber;
+    }
+    public int getIdNumber(){
+        return idNumber;
+    }
+    
+    public void setDepartment(String department){
+        this.department = department;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    
+    public void setPosition(String position){
+        this.position = position;
+    }
+    public String getPosition(){
+        return position;
+    }
+
+}
